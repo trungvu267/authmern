56a16b4ed669017aaf6c78a3a2d3e8e2283a9487
TrungVu <vutrung26072001@gmail.com>
2023-01-21 01:12:52 +0700

    Add dashboard page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Routes>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Route index element={<App />} />
       <Route path="sign-in" element={<SignIn />} />
       <Route path="sign-up" element={<SignUp />} />
+      <Route path="dashboard" element={<Dashboard />} />
       {/* </Route> */}
     </Routes>
   </BrowserRouter>
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../App.css'
+const Dashboard = () => {
+  return <div className="flex">Dashboard</div>
+}
+
+export default Dashboard
