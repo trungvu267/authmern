12f1014674a9bf140cc2b7456b2315b04f04fea8
TrungVu <vutrung26072001@gmail.com>
2023-01-20 23:00:17 +0700

    import index.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react'
+import { getUsers } from './api/userApi'
+import './index.css'
 const App = () => {
+  useEffect(() => {
+    console.log('run')
+    getUsers()
+  }, [])
   return <div>App</div>
 }
 
