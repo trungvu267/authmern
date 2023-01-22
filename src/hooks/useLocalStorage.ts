020d61d32bd46fcb379504c2912263958f8ef4d9
TrungVu <vutrung26072001@gmail.com>
2023-01-23 03:17:36 +0700

    Add custom hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+
+const useLocalStorage = (key: string, defaultValue?: any) => {
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(key)
+
+    if (jsonValue != null) return JSON.parse(jsonValue)
+    if (typeof defaultValue === 'function') {
+      return defaultValue()
+    } else {
+      return defaultValue
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+
+  return [value, setValue]
+}
+
+export default useLocalStorage
